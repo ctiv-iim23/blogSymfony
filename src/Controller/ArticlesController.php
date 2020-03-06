5c24841f97bd1ace91397ae16b7a14f5d876d2d6
alexandre gaillot <alexandre@MacBook-Air-de-alexandre.local>
2020-03-06 16:49:21 +0100

    CRUD Articles

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Articles;
+use App\Entity\Categories;
 use App\Form\ArticlesType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 class ArticlesController extends AbstractController
 {
+
     /**
      * @Route("/articles", name="articles")
      */
@@ -18,14 +20,18 @@ class ArticlesController extends AbstractController
     {
         $newArticle = new Articles();
 
-    $form = $this->createForm(ArticlesType::class, $newArticle);
-
-    $form->handleRequest($request);
-
-
+        $form = $this->createForm(ArticlesType::class, $newArticle);
+        $form->handleRequest($request);
+    
+        $categorieRepository = $this->getDoctrine()
+                            ->getRepository(Categories::class);
 
         if ($form->isSubmitted() && $form->isValid()){
+
+            $categorie = $categorieRepository->find($request->request->get('categorieId'));
+            
             $newArticle = $form->getData();
+            $newArticle->setCategorieId($categorie);
             $newArticle->setCreatedAt(new \DateTime());
 
             $entityManager->persist($newArticle);
@@ -34,12 +40,62 @@ class ArticlesController extends AbstractController
             return $this->redirectToRoute('home');
         }
 
-        $articles = $this->getDoctrine()
-                    ->getRepository(Articles::class)
-                    ->findAll();
+        $articles = $this->articlesRepository = $this->getDoctrine()
+        ->getRepository(Articles::class)->findAll();
+        $categories = $categorieRepository->findAll();
+
         return $this->render('articles/index.html.twig', [
             'articles' => $articles,
-            'articleForm' => $form->createView()
+            'articleForm' => $form->createView(),
+            'categories' => $categories
+        ]);
+    }
+
+    /**
+     * @Route("/articles/remove/{id}", name="removeArticle")
+     */
+    public function remove($id, EntityManagerInterface $entityManager){
+
+        $article = $this->articlesRepository = $this->getDoctrine()
+                        ->getRepository(Articles::class)->find($id);
+        $entityManager->remove($article);
+        $entityManager->flush();
+        return $this->redirectToRoute('articles');
+    }
+
+    /**
+     * @Route("/articles/update/{id}", name="updateArticle")
+     */
+    public function update($id, Request $request, EntityManagerInterface $entityManager){
+
+        $article = $this->getDoctrine()
+                        ->getRepository(Articles::class)->find($id);
+        $categorieRepository = $this->getDoctrine()
+                        ->getRepository(Categories::class);
+        $categories = $categorieRepository->findAll();
+
+        $form = $this->createForm(ArticlesType::class, $article);
+        $form->handleRequest($request);
+
+
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+            $categorie = $categorieRepository->find($request->request->get('categorieId'));
+            
+            $newArticle = $form->getData();
+            $newArticle->setCategorieId($categorie);
+
+            $entityManager->persist($newArticle);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('articles');
+        }
+        
+        return $this->render('articles/update.html.twig', [
+            'article' => $article,
+            'form' => $form->createView(),
+            'categories' => $categories
         ]);
     }
 }
--- src/Form/ArticlesType.php
+++ src/Form/ArticlesType.php
@@ -3,9 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Articles;
-use App\Repository\CategoriesRepository;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,16 +12,10 @@ class ArticlesType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        
-        $categories = $this->getCategories();
 
         $builder
             ->add('title')
-            ->add('description')
-            // ->add('createdAt')
-            ->add('categorieId', ChoiceType::class, [
-                'choices' => $categories
-            ])
+            ->add('description', TextareaType::class)
         ;
     }
 
@@ -31,11 +24,5 @@ class ArticlesType extends AbstractType
         $resolver->setDefaults([
             'data_class' => Articles::class,
         ]);
-    }
-
-    public function getCategories()
-    {
-        $repository = new CategoriesRepository();
-        return $repository->findAll();
     }
 }
